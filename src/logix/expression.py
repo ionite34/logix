bfbfb44b2cc4026fc81e5787fa84aacee6fc7fbb
ionite34 <dev@ionite.io>
2022-08-23 23:01:42 -0400

    Update expression.py

--- src/logix/expression.py
+++ src/logix/expression.py
@@ -1,6 +1,13 @@
 from __future__ import annotations
 
+from dataclasses import dataclass
+
 from symbols import Symbol
+
+
+@dataclass
+class Expression:
+    seq: str
 
 
 class Logic:
@@ -8,7 +15,18 @@ class Logic:
 
     def __init__(self, symbol: str) -> None:
         """Initialize Logic Expression."""
-        # Build symbol
-        sym = Symbol(symbol)
         # Holds all Symbols
-        self.symbols: set[Symbol] = {sym}
+        self._symbols: set[Symbol] = {Symbol(symbol)}
+        # Expression
+        self._exp = Expression(symbol)
+
+    @classmethod
+    def from_expression(cls, exp: Expression) -> Logic:
+        """Creates a Logic from Expression."""
+
+        exp
+
+    def __neg__(self) -> Logic:
+        """Negate Logic Expression."""
+        res = self.copy()
+        return Logic(f"~{self._exp}")
